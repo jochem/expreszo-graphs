a0348ddd787a323856fc30dfdaeb4e0aa8815b24
Jochem Oosterveen <jochem@verre.local>
2014-02-20 10:56:31 +0100

    made this code a little prettier

--- graphs.py
+++ graphs.py
@@ -4,39 +4,45 @@ from rrd.graph import rrdgraph
 app = Flask(__name__)
 graph_types = ('visitors', 'chat', 'members', 'oposts', 'posts', 'topics')
 
+
 @app.route('/graphs/')
 def index():
-	return render_template('graphs/index.html')
+    return render_template('graphs/index.html')
+
 
 @app.route('/graphs/<graph_type>/')
 def detail(graph_type):
-	if graph_type in graph_types:
-		return render_template('graphs/detail.html', graph_type=graph_type)
-	else:
-		abort(404)
+    if not graph_type in graph_types:
+        abort(404)
+    return render_template('graphs/detail.html', graph_type=graph_type)
+
 
 @app.route('/graphs/<graph_type>/<start>.png')
 @app.route('/graphs/<graph_type>/<int:width>/<start>.png')
 def png(graph_type, start=None, width=597):
-	if width < 107:
-		width = 107
-	elif width > 2000:
-		width = 2000
-	width -= 97
-	rg = rrdgraph(str(width))
-	start = str(start)
-	if graph_type in graph_types:
-		image_data = getattr(rg, graph_type)('-' + start, start)
-	else:
-		abort(404)
-	response = make_response(image_data)
-	response.mimetype = "image/png"
-	return response
+    if not graph_type in graph_types:
+        abort(404)
+
+    if width < 107:
+        width = 107
+    elif width > 2000:
+        width = 2000
+    width -= 97
+
+    rg = rrdgraph(str(width))
+    start = str(start)
+    image_data = getattr(rg, graph_type)('-' + start, start)
+
+    response = make_response(image_data)
+    response.mimetype = "image/png"
+    return response
+
 
 @app.errorhandler(404)
 def page_not_found(error):
-	return render_template('404.html'), 404
+    return render_template('404.html'), 404
+
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run()
+    app.debug = True
+    app.run()
